3d8c60d587abaebe6e30893e482a19cff4a7db65
Bryan Gulley <bgulley@rightpoint.com>
2023-02-01 16:46:09 -0600

    tabs exercise

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,29 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
+  // mark clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // find associated tab panel and show it
+  const { id } = event.currentTarget;
+  // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  // tabPanel.hidden = false;
+
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
