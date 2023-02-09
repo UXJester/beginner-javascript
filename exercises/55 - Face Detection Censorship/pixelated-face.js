79eeb88fccfe3628d39c6f568f8c85a565d92a14
Bryan Gulley <bgulley@rightpoint.com>
2023-02-09 12:38:53 -0600

    FINISHED Face detector

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -2,8 +2,22 @@ const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
-const faceCtx = canvas.getContext('2d');
+const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
+const optionsInputs = document.querySelectorAll(
+  '.controls input[type="range"]'
+);
+
+const options = {
+  SIZE: 10,
+  SCALE: 1.25,
+};
+
+function handleOption(event) {
+  const { value, name } = event.currentTarget;
+  options[name] = parseFloat(value);
+}
+optionsInputs.forEach((input) => input.addEventListener('input', handleOption));
 
 // Write a function that will populate the users video
 async function populateVideo() {
@@ -24,7 +38,7 @@ async function detect() {
   const faces = await faceDetector.detect(video);
   // ask the browser when the next animation frame is, and tell it to run detect for us
   faces.forEach(drawFace);
-  // faces.forEach(censor);
+  faces.forEach(censor);
   requestAnimationFrame(detect);
 }
 
@@ -34,6 +48,39 @@ function drawFace(face) {
   ctx.strokeStyle = '#ffc600';
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
+}
+
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  // draw the small face
+  faceCtx.drawImage(
+    // src args
+    video, // image source
+    face.x, //start of source
+    face.y,
+    face.width,
+    face.height,
+    // draw args
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE
+  );
+  // expand small face
+  const width = face.width * options.SCALE;
+  const height = face.height * options.SCALE;
+  faceCtx.drawImage(
+    faceCanvas, //source
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE,
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
 }
 
 populateVideo().then(detect);
